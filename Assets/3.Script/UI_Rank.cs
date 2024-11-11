6c9b91fdfda376486c9b1706b644534b4a26a50e
magicJSC <jeonghaejun004@gmail.com>
2024-11-11 10:32:11 +0900

    랭크

--- Assets/3.Script/UI_Rank.cs
+++ Assets/3.Script/UI_Rank.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI_Rank : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text[] nameTexts;
+    public Text[] scoreTexts;
+
+    public Text recentName;
+    public Text recentScore;
+
+    private void Start()
     {
-        
+        GameManager.Instance.LoadData();
+        for(int i = 0;i< nameTexts.Length; i++)
+        {
+            nameTexts[i].text = GameManager.Instance.playerDataList[i].playerName;
+            scoreTexts[i].text = $"스테이지{GameManager.Instance.playerDataList[i].stageData}";
+        }
+
+        if (GameManager.Instance.playerName != null)
+            recentName.text = GameManager.Instance.playerName;
+        else
+            recentName.text = "--";
+        recentScore.text = $"스테이지{GameManager.Instance.stageData}";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ClickLeave()
     {
-        
+        SceneManager.LoadScene("MainScene");
     }
 }
