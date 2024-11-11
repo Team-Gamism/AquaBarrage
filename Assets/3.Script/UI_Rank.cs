e5b64d8804405b1219b9f028746912af402853fb
magicJSC <jeonghaejun004@gmail.com>
2024-11-11 14:14:53 +0900

    메인 화면

---
+++ Assets/3.Script/UI_Rank.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_Rank : MonoBehaviour
+{
+    public Text[] nameTexts;
+    public Text[] scoreTexts;
+
+    public Text recentName;
+    public Text recentScore;
+
+    private void Start()
+    {
+        GameManager.Instance.LoadData();
+        for (int i = 0; i < nameTexts.Length; i++)
+        {
+            nameTexts[i].text = GameManager.Instance.playerDataList[i].playerName;
+            scoreTexts[i].text = $"스테이지{GameManager.Instance.playerDataList[i].stageData}";
+        }
+
+        if (GameManager.Instance.playerName != null)
+            recentName.text = GameManager.Instance.playerName;
+        else
+            recentName.text = "--";
+        recentScore.text = $"스테이지{GameManager.Instance.stageData}";
+    }
+
+    public void ClickLeave()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
