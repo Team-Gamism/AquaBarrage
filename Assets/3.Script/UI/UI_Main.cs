2ed5e7c5d477934f7ab891a0183e8180137029c0
magicJSC <jeonghaejun004@gmail.com>
2024-11-11 17:24:41 +0900

    상점 UI

---
+++ Assets/3.Script/UI/UI_Main.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_Main : MonoBehaviour
+{
+    public Text nameText;
+
+    private void Start()
+    {
+        GameManager.Instance.InitData();
+    }
+
+    public void ClickPlay()
+    {
+        if (nameText.text != "")
+        {
+            GameManager.Instance.InitData();
+        }
+    }
+
+    public void ClickRank()
+    {
+        SceneManager.LoadScene("RankScene");
+    }
+
+    public void ClickExit()
+    {
+        Application.Quit();
+    }
+}
