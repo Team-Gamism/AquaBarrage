a09f457c98889f7d34332cd4da7ba5dd262f834e
JunghyunByun <bjh1206408@gmail.com>
2024-11-18 20:08:53 +0900

    Update FishingRodController

--- Assets/3.Script/Boat/FishingRodController.cs
+++ Assets/3.Script/Boat/FishingRodController.cs
@@ -6,28 +6,81 @@ using UnityEngine.InputSystem;
 public class FishingRodController : MonoBehaviour
 {
     [SerializeField] private Transform rightShoulderTransform;
+    [SerializeField] private GameObject arrow;
+    [SerializeField] private GameObject launchPrefab;
+    [SerializeField] private Transform launchPoint;
+    [SerializeField] private float arrowSpeed = 45f;
+    [SerializeField] private float launchForce = 100f;
     [SerializeField] private float rotation;
     [SerializeField] private float speed;
     [SerializeField] private float resetSpeed;
     [SerializeField] private float waitTime = 1f;
+    private InputAction castAction;
+    private PlayerInput playerInput;
     private bool isCasting = false;
+    private bool isHoldingCast = false;
+    private float curAngle = 0f;
+    private int dir = 1;
 
-    public void OnCast()
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        castAction = playerInput.actions["Cast"];
+
+        castAction.started += context => StartCasting();
+        castAction.canceled += context => StopCastingAndLaunch();
+    }
+
+    private void OnEnable()
+    {
+        castAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        castAction.Disable();
+    }
+
+    private void StartCasting()
     {
         if (!isCasting)
         {
-            StartCoroutine(CastFishingRod());
+            isCasting = true;
+            isHoldingCast = true;
+            arrow.SetActive(true);
+            StartCoroutine(MoveArrow());
+        }
+    }
+
+    private void StopCastingAndLaunch()
+    {
+        isHoldingCast = false;
+        arrow.SetActive(false);
+        StartCoroutine(CastFishingRod());
+    }
+
+    private IEnumerator MoveArrow()
+    {
+        while (isHoldingCast)
+        {
+            curAngle += arrowSpeed * Time.deltaTime * dir;
+
+            if (curAngle >= 45f || curAngle <= -45f)
+            {
+                dir *= -1;
+            }
+            arrow.transform.localEulerAngles = new Vector3(0, 0, curAngle);
+            yield return null;
         }
     }
 
     private IEnumerator CastFishingRod()
     {
-        isCasting = true;
-
         yield return Rotate(rotation, speed);
         yield return new WaitForSeconds(waitTime);
         yield return Rotate(-180f, resetSpeed);
-
+        yield return new WaitForSeconds(0.1f);
+        LaunchPrefab();
         isCasting = false;
     }
 
@@ -52,5 +105,20 @@ public class FishingRodController : MonoBehaviour
     {
         Vector3 euler = rightShoulderTransform.localEulerAngles;
         rightShoulderTransform.localRotation = Quaternion.Euler(euler.x, euler.y, z);
+    }
+
+    private void LaunchPrefab()
+    {
+        if (launchPrefab != null && launchPoint != null)
+        {
+            GameObject prefab = Instantiate(launchPrefab, launchPoint.position, Quaternion.identity);
+            Rigidbody rb = prefab.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                float angle = curAngle * Mathf.Deg2Rad;
+                Vector3 force = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * launchForce;
+                rb.AddForce(force, ForceMode.Impulse);
+            }
+        }
     }
 }
