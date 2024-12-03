5d1e014e761c49b668e54f4abf7319aa408c8c28
JunghyunByun <bjh1206408@gmail.com>
2024-12-04 02:06:18 +0900

    Update Explosion Audio

--- Assets/3.Script/Fish/Fish/Effect/Explosion.cs
+++ Assets/3.Script/Fish/Fish/Effect/Explosion.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+    [SerializeField] private AudioClip audioClip;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            GameManager.Instance.effectAudioSource.PlayOneShot(audioClip);
             GameManager.Instance.CurHP--;
         }
     }
